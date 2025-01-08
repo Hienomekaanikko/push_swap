9dc43dc115ecae32e917cac927bba4d1a9c0491c
mikkosuokas <mikesuokas@gmail.com>
2025-01-08 09:58:14 +0200

    added the list creation

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,15 +3,8 @@
 
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 #include "libft.h"
 #include "ft_printf.h"
 #include "get_next_line.h"
-
-typedef struct node
-{
-	int	data;
-	struct node	*next;
-} node;
 
 #endif
--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -1,83 +1,48 @@
-#include <stdio.h>
-#include "libft.h"
+#include "push_swap.h"
 
-// Helper function to print the list
-void print_list(t_list *lst)
+void    add_node(t_list **stack, int content)
 {
-    while (lst)
-    {
-        printf("%d -> ", *(int *)lst->content);
-        lst = lst->next;
-    }
-    printf("NULL\n");
-}
+    t_list  *new_node;
+    t_list  *temp;
+    int     *content_copy;
 
-// Delete function for integers
-void del_int(void *content)
-{
-    free(content);
-}
-
-// Function to double the integer value
-void *double_int(void *content)
-{
-    int *new_content = malloc(sizeof(int));
-    if (new_content)
-        *new_content = *(int *)content * 2;
-    return new_content;
-}
-
-// Function to print an integer
-void print_int(void *content)
-{
-    printf("%d ", *(int *)content);
-}
-
-int main(void)
-{
-    t_list *list = NULL;
-    int *num1, *num2, *num3;
-
-    // Create three nodes
-    num1 = malloc(sizeof(int)); *num1 = 10;
-    num2 = malloc(sizeof(int)); *num2 = 20;
-    num3 = malloc(sizeof(int)); *num3 = 30;
-
-    // Add nodes to the list
-    ft_lstadd_back(&list, ft_lstnew(num1));
-    ft_lstadd_back(&list, ft_lstnew(num2));
-    ft_lstadd_front(&list, ft_lstnew(num3));
-
-    printf("Original list: ");
-    print_list(list);
-
-    // Use ft_lstsize
-    printf("List size: %d\n", ft_lstsize(list));
-
-    // Use ft_lstlast
-    t_list *last = ft_lstlast(list);
-    printf("Last element: %d\n", *(int *)last->content);
-
-    // Use ft_lstiter
-    printf("List elements: ");
-    ft_lstiter(list, print_int);
-    printf("\n");
-
-    // Use ft_lstmap
-    t_list *doubled_list = ft_lstmap(list, double_int, del_int);
-    printf("Doubled list: ");
-    print_list(doubled_list);
-
-    // Use ft_lstclear
-    ft_lstclear(&list, del_int);
-    printf("Original list after clearing: ");
-    if (list == NULL)
-        printf("List is empty\n");
+    content_copy = malloc(sizeof(int));
+    if (!content_copy)
+        return ;
+    *content_copy = content;
+    new_node = ft_lstnew(content_copy);
+    if (*stack == NULL)
+        *stack = new_node;
     else
-        print_list(list);
+    {
+        temp = *stack;
+        while (temp->next)
+            temp = temp->next;
+        temp->next = new_node;
+    }
+}
 
-    // Clean up doubled list
-    ft_lstclear(&doubled_list, del_int);
+int main(int argc, char *argv[])
+{
+    int     i;
+    t_list  *stack_a;
+    t_list  *temp;
 
-    return 0;
+    i = 1;
+    stack_a = NULL;
+    if (argc > 1)
+    {
+        while (argv[i])
+        {
+            add_node(&stack_a, ft_atoi(argv[i]));
+            i++;
+        }
+    }
+    temp = stack_a;
+    while (temp)
+    {
+        ft_printf("%d ", *(int *)temp->content);
+        temp = temp->next;
+    }
+    return (0);
 }
